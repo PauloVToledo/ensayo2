4fa47ba2b0a660f0cfdd86ebb9b5a3b3a3f71d86
PauloVToledo <paulovicentetoledosalgado@gmail.com>
2023-11-22 22:01:01 -0300

    finaaal

---
+++ src/main/java/com/example/ensayo2/okkaa.java
@@ -0,0 +1,10 @@
+package com.example.ensayo2;
+
+public class okkaa {
+    //esto es un comentario.
+
+
+    public static void  String(){
+        System.out.println("holaaaaa soy el metedo final.");
+    }
+}
